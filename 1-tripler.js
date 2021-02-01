38a568afacabca08c7dfe5109e031991a0277da1
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-01 13:07:16 -0800

    notes

--- 1-tripler.js
+++ 1-tripler.js
@@ -7,7 +7,39 @@ Examples:
 tripler([1,2,3]); // => [ 3, 6, 9 ]
 tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
+// incoming array: [4, 1, 7] 
 
 function tripler(array) {
-  
+  // initialize the empty array
+  const output = [];
+
+  // loop through the incoming array
+  // use the index to access the elements inside the incoming array
+  for (let i = 0; i < array.length; i++) {
+    //grabs element in that position as opposed to console.log(i) which just grabs the position
+    // console.log(array[i] * 3);
+    const tripled = array[i] * 3;
+    output.push(tripled);
+    // array begins empty, replaced index with output from array[i] * 3 
+    // output[i] = array[i] * 3
+
+  }
+
+  // on each element in the incoming array, multiply it by three
+  // push the tripled numbers into the output
+
+  // return the no-longer-empty array
+  return output;
 }
+
+// count from 20 to 100 in multiples of 5
+for (let i = 20; i <= 100; i+= 5) {
+    console.log(i)
+}
+
+
+let myVar = 19
+
+myVar += 2
+//same as
+myVar = myVar + 2
--- 3-is-prime.js
+++ 3-is-prime.js
@@ -10,8 +10,36 @@ isPrime(11); // => true
 isPrime(9); // => false
 isPrime(2017); // => true
 ***************************************************************************/
+// even numbers are prime because they have >1 factor
 
 function isPrime(number) {
+    for (let i=2; i < number; i++) {
+        // check that the correct thing is targeted 
+        // console.log(i);
+        if (number % i === 0) {
+            // return number, not prime
+            return false            
+        }
+    }  
+    return true
+    // return controls flow of the function (rest of the function is ignored)
 
+    // divide the number by all the numbers leading up to it
+    // if any of those return a whole number, not it is not prime
+    // if we get this far without a NO, then YES 
 }
 
+primeUnderHundo = []
+for (let i = 0; i <100; i++) {
+    if (isPrime(i)) {
+        primeUnderHundo.push(i)
+    }
+}
+
+for (let i in array) {
+    console.log(i); // index: 0, 1 , 2, 3... (i stands for 'index')
+}
+
+for (let i of array) {
+    console.log(i); // object: 3, 1, 7 ... (o stands for 'object') 
+}
